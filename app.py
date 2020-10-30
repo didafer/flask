95e1e5e77025d43322afc38583cdbbbeac9409d2
didafer <dida.fernando@gmail.com>
2020-10-30 20:11:47 +0000

    add templates, static/styles

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', lista=range(15))
 
 @app.route('/<nome>')
 def ola_com_nome(nome):
