f36c476680e48f9c973d6e67856f86f973211785
didafer <dida.fernando@gmail.com>
2020-10-30 19:41:13 +0000

    rotas e variáveis e templates

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello World!!!!</h1>"
+    return render_template('index.html')
+
+@app.route('/<nome>')
+def ola_com_nome(nome):
+    return render_template('index.html', nome_pessoa = nome)
+
+#pip install flask
+##pip install python-dotenv
